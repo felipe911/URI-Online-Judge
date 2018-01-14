05dc7238e44023062fe41f7b5c844577e9e42821
felipe911 <felipe20911@gmail.com>
2018-01-14 14:38:41 -0200

    Algoritmos Uri

---
+++ 1157.c
@@ -0,0 +1,30 @@
+/* URI Online Judge | 1157
+Divisores I
+Adaptado por Neilor Tonin, URI  Brasil
+Timelimit: 1
+Ler um número inteiro N e calcular todos os seus divisores.
+
+Entrada
+
+O arquivo de entrada contém um valor inteiro.
+
+Saída
+
+Escreva todos os divisores positivos de N, um valor por linha. */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(){
+
+    int n, i;
+
+    scanf("%d", &n);
+    for(i=1; i <= n; i++){
+
+        if(n % i == 0){
+            printf("%d\n", i);
+        }
+    }
+    return 0;
+}
